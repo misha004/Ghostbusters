9e0f582ba571c237197cc99ee4a2d8f7f6b2bb67
misha004 <mishakotolup@yandex.ru>
2023-11-08 20:48:40 +0700

    добавил noscroll при попапе

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-//
+const burger = document.querySelector('#button');
+const popup = document.querySelector('#popup');
+const nav = document.querySelector('#nav').cloneNode(1);
+const body = document.body;
+
+burger.addEventListener('click', burgerHandler);
+
+   function burgerHandler(){
+      burger.classList.toggle('active');
+      popup.classList.toggle('open');
+      body.classList.toggle('noscroll');
+      renderPopup();
+   }
+
+   function renderPopup(){
+      popup.appendChild(nav);
+   }
